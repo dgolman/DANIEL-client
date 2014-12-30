63b593dacc0a0452fcfbb1c30e94d190aa080aa9
dgolman <dgolman@vt.edu>
2014-12-30 10:18:23 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name doorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the doorApp
+ */
+angular.module('doorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

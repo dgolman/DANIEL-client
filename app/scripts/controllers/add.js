0748ecf5b3a3e7b4db4096816f92f936a3d97f82
dgolman <dgolman@vt.edu>
2014-12-30 21:35:42 +0200

    admin modal

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -12,48 +12,6 @@ angular.module('doorApp')
     
     filepicker.setKey("AFxAZKXCOSgaCvOFwcdwCz");
 
-    var streaming = false,
-        video        = document.querySelector('#video'),
-        canvas       = document.querySelector('#canvas'),
-        startbutton  = document.querySelector('#startbutton'),
-        uploadbutton = document.querySelector('#uploadbutton'),
-        width = 350,
-        height = 0;
-
-    navigator.getMedia = ( navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia ||
-                           navigator.msGetUserMedia);
-
-    navigator.getMedia(
-      {
-        video: true,
-        audio: false
-      },
-      function(stream) {
-        if (navigator.mozGetUserMedia) {
-          video.mozSrcObject = stream;
-        } else {
-          var vendorURL = window.URL || window.webkitURL;
-          video.src = vendorURL.createObjectURL(stream);
-        }
-        video.play();
-      },
-      function(err) {
-        console.log("An error occured! " + err);
-      }
-    );
-
-    video.addEventListener('canplay', function(ev){
-      if (!streaming) {
-        height = video.videoHeight / (video.videoWidth/width);
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        streaming = true;
-      }
-    }, false);
 
     $scope.takepicture = function() {
       canvas.width = width;
--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -10,6 +10,100 @@
 angular.module('doorApp')
   .controller('AdminCtrl', function ($scope) {
     
+  	$scope.takingPicture = true;
+  	$scope.loading = false;
+ 	var width = 350;
+    var height = 0;
+    filepicker.setKey("AFxAZKXCOSgaCvOFwcdwCz");
 
+    $scope.takepicture = function() {
+      $scope.loading = true;
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+      var data = canvas.toDataURL('image/png').split(',', 2)[1];
+      filepicker.store(
+        data,
+        {
+          mimetype: 'image/png',
+          base64decode: true
+        },
+        function(new_blob){
+          console.log(new_blob.url);
+          $scope.url = new_blob.url;
+          $scope.takingPicture = false;	
+          $scope.loading = false;
+          $scope.$apply();
+          //Store url in database and send to server for facial detection
+        }
+      );
+
+    }
+
+    $scope.uploadpicture = function() {
+    	$scope.loading = true;
+      	filepicker.pickAndStore(
+        {
+          mimetype:"image/*",
+          container: 'window',
+          services: ['FACEBOOK','COMPUTER'],
+          openTo: 'FACEBOOK',
+          multiple: false,
+        }, {},
+        function(Blobs){
+          $scope.url = Blobs[0].url;
+          $scope.takingPicture = false;	
+          $scope.loading = false;
+          $scope.$apply();
+          //Store url in database and send to server for facial detection
+        }, function(errors) {
+          console.log(JSON.stringify(errors));
+        }
+      );
+    }
+
+  	$scope.loadCamera = function() {
+
+	    var streaming = false,
+	        video        = document.querySelector('#video'),
+	        canvas       = document.querySelector('#canvas'),
+	        startbutton  = document.querySelector('#startbutton'),
+	        uploadbutton = document.querySelector('#uploadbutton');
+
+	    navigator.getMedia = ( navigator.getUserMedia ||
+	                           navigator.webkitGetUserMedia ||
+	                           navigator.mozGetUserMedia ||
+	                           navigator.msGetUserMedia);
+
+	    navigator.getMedia(
+	      {
+	        video: true,
+	        audio: false
+	      },
+	      function(stream) {
+	        if (navigator.mozGetUserMedia) {
+	          video.mozSrcObject = stream;
+	        } else {
+	          var vendorURL = window.URL || window.webkitURL;
+	          video.src = vendorURL.createObjectURL(stream);
+	        }
+	        video.play();
+	      },
+	      function(err) {
+	        console.log("An error occured! " + err);
+	      }
+	    );
+
+	    video.addEventListener('canplay', function(ev){
+	      if (!streaming) {
+	        height = video.videoHeight / (video.videoWidth/width);
+	        video.setAttribute('width', width);
+	        video.setAttribute('height', height);
+	        canvas.setAttribute('width', width);
+	        canvas.setAttribute('height', height);
+	        streaming = true;
+	      }
+	    }, false);
+  	}
 
   });

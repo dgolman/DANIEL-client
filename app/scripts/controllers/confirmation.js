d541846dac8b1a982f88a7e8b57c22db87f6cc31
Samantha <samgruskin@gmail.com>
2014-12-30 17:08:14 +0200

    badassery

--- app/scripts/controllers/confirmation.js
+++ app/scripts/controllers/confirmation.js
@@ -10,10 +10,17 @@
 angular.module('doorApp')
   .controller('ConfirmationCtrl', function ($scope, URLSession, $http) {
     $scope.image_src = URLSession.url;
+    $scope.name = "";
   	$scope.confirmed = function() {
   		//SEND Url to Server for facial processing
   		//dm.ngrok.com/upload
-  		$http.post('http://daniel.ngrok.com/upload', {img_url: 'https://www.filepicker.io/api/file/gsDm8qYTOuduxitthSIj', name: "Daniel"})
+  		$http.post(
+            'http://daniel.ngrok.com/upload', 
+            {
+                img_url: URLSession.url,
+                name: $scope.name
+            }
+        )
 		.success(function(response) {
   			console.log(response);
   		});

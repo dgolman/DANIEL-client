bbac2846e25a35e0b053b10208dc4272b5bd1c7e
dgolman <dgolman@vt.edu>
2014-12-30 16:16:13 +0200

    URL session

--- app/scripts/controllers/confirmation.js
+++ app/scripts/controllers/confirmation.js
@@ -8,8 +8,15 @@
  * Controller of the doorApp
  */
 angular.module('doorApp')
-  .controller('ConfirmationCtrl', function ($scope) {
+  .controller('ConfirmationCtrl', function ($scope, URLSession, $http) {
     
-
+  	$scope.confirmed = function() {
+  		//SEND Url to Server for facial processing
+  		//dm.ngrok.com/upload
+  		$http.post('http://dm.ngrok.com/upload', {data: URLSession.url, name: "Daniel"})
+		.success(function(response) {
+  			console.log(response);
+  		});
+  	}
 
   });
